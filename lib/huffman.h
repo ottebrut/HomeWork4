0979dd866dfcf629ce612734536242408f78f9d8
ottebrut <48487472+ottebrut@users.noreply.github.com>
2019-06-15 16:30:17 +0300

    Update huffman.h

--- lib/huffman.h
+++ lib/huffman.h
@@ -1,19 +1,11 @@
 #ifndef HUFFMAN_HPP
 #define HUFFMAN_HPP
 
-#include <vector>
-
 typedef unsigned int uint;
 typedef unsigned char uchar;
 typedef unsigned long long ull;
 
-using namespace std;
-
-void compressor(istream &in, ostream &out);
-bool decompressor(istream &in, ostream &out);
-
-template<typename T>
-void write(T symb, ostream &out);
-vector<pair<uint, char>> get_codes(const vector<uint> &cnt);
+void compress(std::istream &in, std::ostream &out);
+bool decompress(std::istream &in, std::ostream &out);
 
 #endif
