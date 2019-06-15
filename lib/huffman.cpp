c1e0395a1392f4d813d65a3f36f3137a05b07cd5
ottebrut <48487472+ottebrut@users.noreply.github.com>
2019-06-15 16:30:04 +0300

    Update huffman.cpp

--- lib/huffman.cpp
+++ lib/huffman.cpp
@@ -12,107 +12,23 @@ using namespace std;
 const int BUFF_SIZE = 4096;
 const int A_SIZE = 256;
 
-uchar buffer[BUFF_SIZE];
-uchar buffer_out[BUFF_SIZE];
-uint buff_ind = 0;
-ull length = 0;
-ull stream = 0;
-
-void compressor(istream &in, ostream &out) {
-
-    vector<uint> cnt(A_SIZE);
-
-    in.seekg(0);
-    size_t size;
-    while((size = in.readsome((char *)buffer, BUFF_SIZE)) > 0) {
-        for(size_t ind = 0; ind < size; ++ind) {
-            cnt[buffer[ind]]++;
-        }
-    }
-
-    out.write((char *)&stream, sizeof(stream));
-    buff_ind = 0;
-    for(size_t ind = 0; ind < A_SIZE; ++ind) {
-        write((uchar)ind, out);
-        write(cnt[ind], out);
-    }
-
-    vector<pair<uint, char>> code = get_codes(cnt);
-    uint line = 0;
-    uint line_size = 0;
-    stream = 0;
-    in.seekg(0);
-    while((size = in.readsome((char *)buffer, BUFF_SIZE)) > 0) {
-        for(size_t ind = 0; ind < size; ++ind) {
-            line += code[buffer[ind]].first << line_size;
-            line_size += code[buffer[ind]].second;
-            if (line_size >= 32) {
-                write(line, out);
-                line_size -= 32;
-                line = code[buffer[ind]].first >> (code[buffer[ind]].second - line_size);
-            }
-        }
-    }
-    if (line_size) {
-        write(line, out);
-        stream -= 32 - line_size;
-    }
-
-    if (buff_ind) {
-        out.write((char *)buffer_out, buff_ind);
-    }
-    out.seekp(0);
-    out.write((char *)&stream, sizeof(stream));
-}
-
-bool decompressor(istream &in, ostream &out) {
-
-    vector<uint> cnt(A_SIZE);
-
-    in.seekg(0);
-    in.read((char *)&length, sizeof(length));
-    for(size_t ind = 0; ind < A_SIZE; ++ind) {
-        uchar i;
-        in.read((char *)&i, sizeof(i));
-        if (i != ind) {
-            return false;
-        }
-        in.read((char *)&cnt[ind], sizeof(cnt[ind]));
-    }
-
-    tree Tree(get_codes(cnt));
-    buff_ind = 0;
-    size_t size;
-    while((size = in.readsome((char *)buffer, BUFF_SIZE)) > 0) {
-        for(size_t ind = 0; ind < size; ++ind) {
-            for(size_t b = 0; b < min((ull)8, length); ++b) {
-                if (Tree.go((buffer[ind] >> b) & 1u)) {
-                    if (Tree.end()) {
-                        write(Tree.get(), out);
-                    }
-                } else {
-                    return false;
-                }
-            }
-            length = (length > 8 ? length - 8 : 0);
-        }
-    }
-
-    if (buff_ind) {
-        out.write((char *)buffer_out, buff_ind);
-    }
-    return true;
-}
+struct _buff {
+    uchar buffer[BUFF_SIZE];
+    uchar buffer_out[BUFF_SIZE];
+    uint buff_ind = 0;
+    ull length = 0;
+    ull stream = 0;
+};
 
 template<typename T>
-void write(T symb, ostream &out) {
-    if (buff_ind + sizeof(symb) >= BUFF_SIZE) {
-        out.write((char *)buffer_out, buff_ind);
-        buff_ind = 0;
+void write(T symb, ostream &out, _buff &buff) {
+    if (buff.buff_ind + sizeof(symb) >= BUFF_SIZE) {
+        out.write((char *)buff.buffer_out, buff.buff_ind);
+        buff.buff_ind = 0;
     }
-    memcpy(buffer_out + buff_ind, &symb, sizeof(symb));
-    buff_ind += sizeof(symb);
-    stream += 8 * sizeof(symb);
+    memcpy(buff.buffer_out + buff.buff_ind, &symb, sizeof(symb));
+    buff.buff_ind += sizeof(symb);
+    buff.stream += 8 * sizeof(symb);
 }
 
 vector<pair<uint, char>> get_codes(const vector<uint> &cnt) {
@@ -148,4 +64,92 @@ vector<pair<uint, char>> get_codes(const vector<uint> &cnt) {
     }
 
     return res;
+}
+
+void compress(istream &in, ostream &out) {
+
+    _buff buff;
+    vector<uint> cnt(A_SIZE);
+
+    in.seekg(0);
+    size_t size;
+    while((size = in.readsome((char *)buff.buffer, BUFF_SIZE)) > 0) {
+        for(size_t ind = 0; ind < size; ++ind) {
+            cnt[buff.buffer[ind]]++;
+        }
+    }
+
+    out.write((char *)&buff.stream, sizeof(buff.stream));
+    buff.buff_ind = 0;
+    for(size_t ind = 0; ind < A_SIZE; ++ind) {
+        write((uchar)ind, out, buff);
+        write(cnt[ind], out, buff);
+    }
+
+    vector<pair<uint, char>> code = get_codes(cnt);
+    uint line = 0;
+    uint line_size = 0;
+    buff.stream = 0;
+    in.seekg(0);
+    while((size = in.readsome((char *)buff.buffer, BUFF_SIZE)) > 0) {
+        for(size_t ind = 0; ind < size; ++ind) {
+            line += code[buff.buffer[ind]].first << line_size;
+            line_size += code[buff.buffer[ind]].second;
+            if (line_size >= 32) {
+                write(line, out, buff);
+                line_size -= 32;
+                line = code[buff.buffer[ind]].first >> (code[buff.buffer[ind]].second - line_size);
+            }
+        }
+    }
+    if (line_size) {
+        write(line, out, buff);
+        buff.stream -= 32 - line_size;
+    }
+
+    if (buff.buff_ind) {
+        out.write((char *)buff.buffer_out, buff.buff_ind);
+    }
+    out.seekp(0);
+    out.write((char *)&buff.stream, sizeof(buff.stream));
+}
+
+bool decompress(istream &in, ostream &out) {
+
+    _buff buff;
+    vector<uint> cnt(A_SIZE);
+
+    in.seekg(0);
+    in.read((char *)&buff.length, sizeof(buff.length));
+    for(size_t ind = 0; ind < A_SIZE; ++ind) {
+        uchar i;
+        in.read((char *)&i, sizeof(i));
+        if (i != ind) {
+            return false;
+        }
+        in.read((char *)&cnt[ind], sizeof(cnt[ind]));
+    }
+
+    tree Tree(get_codes(cnt));
+    buff.buff_ind = 0;
+    size_t size;
+    while((size = in.readsome((char *)buff.buffer, BUFF_SIZE)) > 0) {
+        for(size_t ind = 0; ind < size; ++ind) {
+            for(size_t b = 0; b < min((ull)8, buff.length); ++b) {
+                if (Tree.go((buff.buffer[ind] >> b) & 1u)) {
+                    if (Tree.end()) {
+                        write(Tree.get(), out, buff);
+                    }
+                } else {
+                    return false;
+                }
+            }
+            buff.length = (buff.length > 8 ? buff.length - 8 : 0);
+        }
+    }
+
+    if (buff.buff_ind) {
+        out.write((char *)buff.buffer_out, buff.buff_ind);
+    }
+    return true;
 }
